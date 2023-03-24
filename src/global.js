9f18181df319285bf1ebd43868ca78601400ec5b
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-03-24 16:05:53 +0530

    render deploy

--- src/global.js
+++ src/global.js
@@ -1,4 +1,6 @@
 // export const API = "https://63d75fb7afbba6b7c93beb15.mockapi.io"
 // export const API = "http://localhost:4000"
 
-export const API = "https://mongo-node-r2xq2fpp9-abinithishkumarprabakaran.vercel.app"
+// export const API = "https://mongo-node-r2xq2fpp9-abinithishkumarprabakaran.vercel.app"
+
+export const API = "https://movies-api-3cg6.onrender.com"
